af3c2bec0eecc3cfb9a244ef89bd080337d63205
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-04-25 22:56:50 +0530

    Added NewCollections section

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import "./Popular.css";
-import data_product from "../Assets/data";
+import "./NewCollections.css";
+import new_collection from "../Assets/new_collections";
 import Item from "../Item/Item";
 
-const Popular = () => {
+const NewCollections = () => {
   return (
-    <div className="popular">
-      <h1>POPULAR IN WOMEN</h1>
+    <div className="new-collections">
+      <h1>NEW COLLECTIONS</h1>
       <hr />
-      <div className="popular-item">
-        {data_product.map((item, i) => {
+      <div className="collections">
+        {new_collection.map((item, i) => {
           return (
             <Item
               key={i}
@@ -26,4 +26,4 @@ const Popular = () => {
   );
 };
 
-export default Popular;
+export default NewCollections;
