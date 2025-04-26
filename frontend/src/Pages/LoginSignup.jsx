ef9fa010e49d6a5ab8099a5b17377468f7d48354
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-04-26 18:42:19 +0530

    Added Login Signup section

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
   return (
-    <div>
-        
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>
+            By continuing, I agree to Shopper's Terms of Use and Privacy Policy
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
