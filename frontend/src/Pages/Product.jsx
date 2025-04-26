a259471114eec3cafa71a6652b31bb2ad5651d14
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-04-26 19:22:46 +0530

    Added Breadcrumbs

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import { useParams } from "react-router-dom";
 
 const Product = () => {
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId));
   return (
     <div>
-        
+      <Breadcrum product={product} />
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
